085edfcbac41080f6769e008fb199a197e2b1aa9
Ancients <41245310+AncientLaws@users.noreply.github.com>
2021-10-17 06:01:01 +0300

    Adding Runescape commit

--- Cypods/src/main_pod/Run.java
+++ Cypods/src/main_pod/Run.java
@@ -11,7 +11,8 @@ public class Run {
 		//GET GET1 = new GET("https://services6.arcgis.com/bKYAIlQgwHslVRaK/arcgis/rest/services/DailyTestPerformance_ViewLayer/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json","ReportDate");
 		//GET Get2 = new GET("https://icanhazdadjoke.com/","joke");
 			System.out.println("Ran");
-		GET Get3 = new GET("https://chroniclingamerica.loc.gov/search/pages/results/?format=json", new int []  {0,1,2,3});
+		//GET Get3 = new GET("https://chroniclingamerica.loc.gov/search/pages/results/?format=json", new int []  {0,1,2,3});
+		GET rs = new GET("https://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=21787");
 		
 }
 }}
