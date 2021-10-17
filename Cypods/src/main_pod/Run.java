1012173c864f5d951c08245ee0c5758d38683d2b
AncientLaws <bo8213@gmail.com>
2021-10-17 03:02:16 +0300

    First commit of Cypods

---
+++ Cypods/src/main_pod/Run.java
@@ -0,0 +1,17 @@
+////////////////////////
+package main_pod;
+
+public class Run {
+
+	public static void main (String args []){
+	{
+		
+		
+		//GET Get1 = new GET("https://chroniclingamerica.loc.gov/search/pages/results/?format=json");
+		//GET GET1 = new GET("https://services6.arcgis.com/bKYAIlQgwHslVRaK/arcgis/rest/services/DailyTestPerformance_ViewLayer/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json","ReportDate");
+		//GET Get2 = new GET("https://icanhazdadjoke.com/","joke");
+			System.out.println("Ran");
+		GET Get3 = new GET("https://chroniclingamerica.loc.gov/search/pages/results/?format=json", new int []  {0,1,2,3});
+		
+}
+}}
