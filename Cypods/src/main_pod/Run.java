af4d3ee98baa404c9dab2656c425cca00eff59e2
Ancients <41245310+AncientLaws@users.noreply.github.com>
2022-01-04 11:31:38 +0300

    Adding JavaFX

--- Cypods/src/main_pod/Run.java
+++ Cypods/src/main_pod/Run.java
@@ -1,7 +1,7 @@
 ////////////////////////
 package main_pod;
 
-public class Run {
+public class Run extends Window{
 
 	public static void main(String args[]) {
 
@@ -10,7 +10,6 @@ public class Run {
 		// GET GET1 = new
 		// GET("https://services6.arcgis.com/bKYAIlQgwHslVRaK/arcgis/rest/services/DailyTestPerformance_ViewLayer/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json","ReportDate");
 		// GET Get2 = new GET("https://icanhazdadjoke.com/","joke");
-		System.out.println("Ran");
 		// GET Get3 = new
 		// GET("https://chroniclingamerica.loc.gov/search/pages/results/?format=json",
 		// new int [] {0,1,2,3});
@@ -19,5 +18,9 @@ public class Run {
 		// GET rs2 = new
 		// GET("https://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=21787","item");
 		GET rs3 = new GET("http://192.168.1.157:3000/root");
+
+		
+		launch();
+		
 	}
 }
