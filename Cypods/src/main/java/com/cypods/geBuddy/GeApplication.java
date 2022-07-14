1a8e5c81bcc31d2d7448e9028452f9c063e67e25
Ancients <41245310+AncientLaws@users.noreply.github.com>
2022-07-14 03:56:46 +0300

    Pushing all

---
+++ Cypods/src/main/java/com/cypods/geBuddy/GeApplication.java
@@ -0,0 +1,13 @@
+package com.cypods.geBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeApplication.class, args);
+	}
+
+}
